c01dd093323d5bd88ac6db3efa4088f8a5acdca1
Pitkänen Santeri J <pitkasan@cubbli22-gold.local>
2024-02-21 17:49:43 +0200

    remove __pycache__ from git

--- fakeGram/settings.py
+++ fakeGram/settings.py
@@ -29,10 +29,10 @@ DEBUG = True
 ALLOWED_HOSTS = []
 
 
-# Application definition
+# uncomment sslserver to use https
 
 INSTALLED_APPS = [
-    'sslserver',
+    #'sslserver',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
